ae2ebf2e559c8b8168021bd1494c01f6d2d9c1b5
Amit Prabhu <amitcprabhu@gmail.com>
2018-06-18 22:08:12 +0530

    changed UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
-import { MatButtonModule, MatCheckboxModule, MatInputModule , MatCardModule, MatSelectModule} from '@angular/material';
-import { HttpClientModule
-} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HTTPService } from './Http-service.service';
 
@@ -20,13 +18,6 @@ import { HTTPService } from './Http-service.service';
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule
-,
-    // material modules
-    MatButtonModule, 
-    MatCheckboxModule,
-    MatCardModule,
-    MatInputModule,
-    MatSelectModule
   ],
   providers: [HTTPService],
   bootstrap: [AppComponent]
